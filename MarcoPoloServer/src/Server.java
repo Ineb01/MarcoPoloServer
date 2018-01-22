34aed9038a7350c15eac8accbc14967cf2a3987a
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2018-01-22 18:03:06 +0100

    Bugfixes and end, endSocket commands

--- MarcoPoloServer/src/Server.java
+++ MarcoPoloServer/src/Server.java
@@ -10,58 +10,69 @@ import java.util.regex.Pattern;
 public class Server {
 
 	private ServerSocket server;
-	
+
 	private boolean running = true;
 
 	public Server(int port) {
 		try {
 			server = new ServerSocket(1254);
-
+			
 			while (running) {
 
 				Socket socket = server.accept();
-				new ActionThread(socket).start();
+				ActionThread action = new ActionThread(socket);
+				action.start();
+				
+				running = action.getRunning();
 
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-	}
-	
-	private class ActionThread extends Thread{
-		public ActionThread(Socket socket) {
-			try {
-				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-				
-				String message = input.readLine();
-				
-				Pattern p = Pattern.compile("[mM]arco");
-				Matcher m = p.matcher(message);
-				
-				System.out.println(message);
-				
-				while(m.find()) {
-					output.println("Polo");
-				}
-				
-				if(Pattern.compile("end").matcher(message).find()) {
-					running = false;
-				}
-				
-				socket.close();
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	public static void main(String[] args) {
 		new Server(1254);
 	}
 
+}
+
+class ActionThread extends Thread {
+	private boolean running = true;
+	public ActionThread(Socket socket) {
+		try {
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			boolean requestRunning = true;
+			while (requestRunning) {
+				String message = input.readLine();
+				if (message != null) {
+					Pattern p = Pattern.compile("[Mm]arco");
+					Matcher m = p.matcher(message);
+
+					System.out.println(message);
+
+					while (m.find()) {
+						output.println("Polo");
+					}
+
+					if (Pattern.compile("[Ee]nd[Ss]ocket").matcher(message).find()) {
+						requestRunning = false;
+						running = false;
+					} else if (Pattern.compile("[Ee]nd").matcher(message).find()) {
+						requestRunning = false;
+					}
+				}
+			}
+			socket.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public boolean getRunning() {
+		
+		return running;
+	}
 }
--- MarcoPoloServer/src/TestClient.java
+++ MarcoPoloServer/src/TestClient.java
@@ -14,26 +14,33 @@ public class TestClient {
 			Socket socket = new Socket("localhost", 1254);
 			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-//			new Thread() {
-//				@Override
-//				public void run() {
-//					try {
-//						BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//						String message = " ";
-//						do{
-//							System.out.println(message);
-//							message = input.readLine();
-//						}while(message != null);
-//					} catch (IOException e) {
-//						e.printStackTrace();
-//					}
-//					super.run();
-//				}
-//			}.start();
-			while (true) {
-				String keyboardInput = keyboard.readLine();
+			Thread writeinput = new Thread() {
+				@Override
+				public void run() {
+					try {
+						BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						String message;
+						do{
+							message = input.readLine();
+							System.out.println(message);
+						}while(message != null);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					super.run();
+				}
+			};
+			writeinput.start();
+			String keyboardInput;
+			do{
+				keyboardInput = keyboard.readLine();
+				if(Pattern.compile("[Ee]nd").matcher(keyboardInput).find()) {
+					output.println("SocketEnd");
+					writeinput.stop();
+					socket.close();
+				}
 				output.println(keyboardInput);
-			}
+			}while(!Pattern.compile("[Ee]nd").matcher(keyboardInput).find());
 
 		} catch (UnknownHostException e) {
 			System.out.println("unknown host");
